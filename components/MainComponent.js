185619fad556ba709df5c1dcbcd0fd8a2faabbf9
Andrew Davidson <andrew.kilgore.davidson@gmail.com>
2019-09-30 21:20:13 -0700

    'Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishDetailComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoriteComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import {
@@ -185,6 +186,33 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          color="white"
+          onPress={() => {
+            navigation.toggleDrawer();
+          }}
+        />
+      )
+    })
+  }
+);
+
 const CustomDrawerContentComponent = props => (
   <ScrollView>
     <SafeAreaView
@@ -256,6 +284,16 @@ const MainNavigator = createDrawerNavigator(
             size={22}
             color={tintColor}
           />
+        )
+      }
+    },
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        title: "My Favorites",
+        drawerLabel: "My Favorites",
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="heart" type="font-awesome" size={24} color={tintColor} />
         )
       }
     },
